d000c3f64df2e4f2381ab0b8289ce97112b2faf9
Misael <misaelnodaquintero@gmail.com>
2017-05-22 09:28:51 +0200

    Añadiendo formulario

--- PROYECTOLM.py
+++ PROYECTOLM.py
@@ -58,18 +58,9 @@ def movimientos(cuentaid) :
 	if fechafrom and fechato :
 		r4=requests.get("https://apis.bbva.com/accounts-sbx/v1/me/accounts/"+cuentaid+"/transactions?dateFrom="+fechafrom+"&dateTo="+fechato,headers=cabecerar4)
 		json=r4.json()
-		return fechafrom,fechato
-#		return template("infotransacciones.tpl",movimientos=json["data"]["accountTransactions"])
-	elif fechafrom :
-		r4=requests.get("https://apis.bbva.com/accounts-sbx/v1/me/accounts/"+cuentaid+"/transactions?dateFrom="+fechafrom,headers=cabecerar4)
-		json=r4.json()
-		return fechafrom,fechato
-#		return template("infotransacciones.tpl",movimientos=json["data"]["accountTransactions"])
+		return template("infotransacciones.tpl",movimientos=json["data"]["accountTransactions"])
 	else :
-		r4=requests.get("https://apis.bbva.com/accounts-sbx/v1/me/accounts/"+cuentaid+"/transactions",headers=cabecerar4)
-		json=r4.json()
-		return fechafrom,fechato
-#		return template("infotransacciones.tpl",movimientos=json["data"]["accountTransactions"])
+		return <h1>Necesitas introducir las dos fechas, o ninguna</h1>
 
 @route('/desloguearse')
 def desloguearse() :
